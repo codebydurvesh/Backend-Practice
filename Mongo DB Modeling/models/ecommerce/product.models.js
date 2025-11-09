37824c2dd8bb6b16d2ff1336c8a8a060e6f589a1
codebydurvesh <durvesh.gaikwad08@gmail.com>
2025-11-09 10:36:06 +0530

    Data Modelling: 02

---
+++ Mongo DB Modeling/models/ecommerce/product.models.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import { Category } from "./category.models";
+
+const productSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      requried: true,
+    },
+
+    name: {
+      type: String,
+      requried: true,
+    },
+
+    productImage: {
+      type: String,
+    },
+
+    price: {
+      type: Number,
+      default: 0,
+    },
+
+    stock: {
+      type: Number,
+      default: 0,
+    },
+
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+
+  },
+  { timestamps: true }
+);
+
+export const Product = mongoose.model("Product", productSchema);
