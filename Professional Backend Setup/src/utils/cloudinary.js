e36671c7513df38f9c135d2aa9bb308c4f507d31
codebydurvesh <durvesh.gaikwad08@gmail.com>
2025-11-18 09:09:31 +0530

    Professional Backend Setup: Used Multer

---
+++ Professional Backend Setup/src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import { response } from "express";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      return console.log("Could not find the file path!");
+      //upload the file on cloudinary
+      const response = await cloudinary.uploader.upload(localFilePath, {
+        resource_type: "auto",
+      });
+      //uploaded successfully
+      console.log("File is uploaded on Cloudinary", response.url);
+      return response;
+    }
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // removes the locally saved temp file as the uploaded operation got failed
+    return null;
+  }
+};
+
+export {uploadOnCloudinary}
